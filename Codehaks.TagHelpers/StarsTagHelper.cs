2a530f5d0d521f1a62096ec5c5c95fa7cad2922c
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-29 21:00:17 +0330

    Stars

--- Codehaks.TagHelpers/StarsTagHelper.cs
+++ Codehaks.TagHelpers/StarsTagHelper.cs
@@ -7,10 +7,22 @@ namespace Codehaks.TagHelpers
         public StarsTagHelper()
         {
             Count = 5;
+            ActiveClass = "fa fa-star";
+            ActiveStyle = "color:#ffd800";
+
+            DeActiveClass = "fa fa-star-o";
+            DeActiveStyle = "";
+
         }
         public int Level { get; set; }
 
         public int Count { get; set; }
+
+        public string ActiveClass { get; set; }
+        public string ActiveStyle { get; set; }
+
+        public string DeActiveClass { get; set; }
+        public string DeActiveStyle { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -18,12 +30,11 @@ namespace Codehaks.TagHelpers
             {
                 if (i < Level)
                 {
-
-                    output.Content.AppendHtml("<i class='fa fa-star' style='color:#ffd800'></i>");
+                    output.Content.AppendHtml("<i class='"+ ActiveClass+"' style='" + ActiveStyle + "'></i>");
                 }
                 else
                 {
-                    output.Content.AppendHtml("<i class='fa fa-star-o'></i>");
+                    output.Content.AppendHtml("<i class='" +DeActiveClass + "' style='" + DeActiveStyle + "'></i>");
                 }
             }
         }
